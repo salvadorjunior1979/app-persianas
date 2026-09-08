145e9685ec77342496f408de70f8b699180e3e7b
salvadorjunior1979 <persianasdinizsf@hotmail.com>
2026-09-08 10:35:01 -0300

    Update service worker for caching and activation

--- sw.js
+++ sw.js
@@ -1,19 +1,34 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('persianas-app-v1').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
-    })
+const CACHE_NAME = 'persianas-ra-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting(); // Força o novo Service Worker a ativar imediatamente
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); // Deleta o cache antigo (v1)
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
